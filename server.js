62d85a8fba2fbdbe898f3c26d908edc3bddbb397
Dan Alvares <dm.alvares@gmail.com>
2019-02-28 22:27:11 +0000

    Seperate schema and resolvers

--- server.js
+++ server.js
@@ -1,66 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql');
-const { buildSchema } = require('graphql');
 const mongoose = require('mongoose');
 
 const app = express();
 
-const events = []
+const schema = require('./graphql/schema');
+const rootValue = require('./graphql/resolvers');
 
 app.use(bodyParser.json());
 
 app.use(
     '/graphql',
     graphqlHttp({
-        schema: buildSchema(`
-            type Event {
-                _id: ID!
-                title: String!
-                description: String!
-                price: Float!
-                date: String!
-            }
-
-            input EventInput {
-                title: String!
-                description: String!
-                price: Float!
-                date: String!
-            }
-
-            type RootQuery {
-                events: [Event!]!
-            }
-
-            type RootMutation {
-                createEvent(eventInput: EventInput): Event
-            }
-
-            schema {
-                query: RootQuery
-                mutation: RootMutation
-            }
-        `),
-        rootValue: {
-            events: () => events,
-            createEvent: args => {
-                const event = {
-                    _id: Math.random().toString(),
-                    title: args.eventInput.title, 
-                    description: args.eventInput.description, 
-                    price: +args.eventInput.price, 
-                    date: new Date().toISOString()
-                };
-                events.push(event);
-                return event;
-            }
-        },
+        schema,
+        rootValue,
         graphiql: true
     })
 );
 
-mongoose.connect(`mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@ds351455.mlab.com:51455/graphql-events`)
+mongoose.connect(`mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@ds351455.mlab.com:51455/${process.env.MONGO_DB}`)
     .then(() => {})
     .catch((err) => {
         console.log(err)
